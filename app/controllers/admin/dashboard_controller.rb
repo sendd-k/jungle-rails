19f829c369d6e65d1c7dc34f5a762f78d6c8c735
Kevin Holmes <kevinholmes921@gmail.com>
2022-06-29 23:53:16 +0000

    removing username/passwrod from codebase into env file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
   end
 end
